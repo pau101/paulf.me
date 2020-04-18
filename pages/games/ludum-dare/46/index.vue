b205281dba32f4fb778e9161dc1a97d0284c6b39
Paul Fulham <paul.fulham0@gmail.com>
2020-04-18 10:00:39 -0700

    Now Babylon.js

--- pages/games/ludum-dare/46/index.vue
+++ pages/games/ludum-dare/46/index.vue
@@ -2,13 +2,13 @@
   <main aria-role="main">
     <div>
       <h1>Ludum Dare 46</h1>
-      <div id="game" :style="{ height: height + 'px' }"></div>
+      <canvas id="game" :width="width" :height="height"></canvas>
     </div>
   </main>
 </template>
 
 <script>
-import * as THREE from 'three'
+import * as BABYLON from 'babylonjs'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import * as KeyCode from 'keycode-js'
 
@@ -17,94 +17,86 @@ export default {
     return {
       width: 1088,
       height: 612,
-      scene: undefined,
-      camera: undefined,
-      renderer: undefined,
-      cube: undefined,
-      plane: undefined,
-      controls: undefined,
-      up: false,
-      down: false,
-      left: false,
-      right: false,
-      raycaster: undefined
+      engine: undefined,
+      scene: undefined
     }
   },
   mounted() {
-    const e = document.getElementById('game')
-    this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      this.width / this.height,
-      0.1,
-      1000
+    const canvas = document.getElementById('game')
+    this.engine = new BABYLON.Engine(canvas, true)
+    this.scene = new BABYLON.Scene(this.engine)
+    const camera = new BABYLON.ArcRotateCamera(
+      'Camera',
+      Math.PI / 4,
+      Math.PI / 3,
+      2,
+      new BABYLON.Vector3(5, 4, 5),
+      this.scene
     )
-    this.camera.position.set(-7, 3, 3)
-    this.camera.lookAt(0, 0, 0)
-    // renderer
-    this.renderer = new THREE.WebGLRenderer({ antialias: true })
-    this.renderer.setPixelRatio(window.devicePixelRatio)
-    this.renderer.setSize(this.width, this.height)
-    e.appendChild(this.renderer.domElement)
-    this.raycaster = new THREE.Raycaster()
-    // this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    // scene
-    // this.scene.add(new THREE.AxesHelper(4))
-    this.cube = new THREE.Mesh(
-      new THREE.BoxGeometry(0.4, 1.0, 0.4),
-      new THREE.MeshLambertMaterial({ color: '#7f3f3f' })
+    camera.attachControl(canvas, true)
+    // Add lights to the scene
+    // eslint-disable-next-line no-new
+    const ambient = new BABYLON.HemisphericLight(
+      'light1',
+      new BABYLON.Vector3(1, 1, 0),
+      this.scene
     )
-    this.cube.geometry.computeBoundingBox()
-    this.cube.position.y = 2
-    this.scene.add(this.cube)
-    // plane
-    const size = 32
-    this.plane = new THREE.Mesh(
-      new THREE.PlaneGeometry(16, 16, size, size),
-      new THREE.MeshLambertMaterial({ color: '#7f7f7f' })
+    ambient.intensity = 0.5
+    const sun = new BABYLON.DirectionalLight(
+      'light2',
+      new BABYLON.Vector3(-0.5, -1, 0).normalize(),
+      this.scene
     )
-    this.plane.rotateX(-Math.PI / 2)
-    for (let y = 0; y <= size; y++) {
-      for (let x = 0; x <= size; x++) {
-        this.plane.geometry.vertices[x + y * (1 + size)].z =
-          (Math.cos((x * 19) / size) + Math.cos((y * 14 + x * 13) / size)) * 0.5
-      }
-    }
-    this.plane.geometry.computeVertexNormals()
-    this.plane.geometry.computeBoundingBox()
-    // plane.geometry.computeFaceNormals()
-    this.scene.add(this.plane)
-    // lights
-    const light = new THREE.DirectionalLight()
-    light.position.set(65, 100, 50)
-    this.scene.add(light)
-    this.scene.add(new THREE.AmbientLight('#ffffff', 0.13))
-    // input
+    sun.intensity = 0.5
+    // eslint-disable-next-line no-new
+    // new BABYLON.PointLight(
+    //   'light2',
+    //   new BABYLON.Vector3(0, 10, -10),
+    //   this.scene
+    // )
+    // Add and manipulate meshes in the scene
+    const sphere = BABYLON.MeshBuilder.CreateSphere(
+      'sphere',
+      { diameter: 2 },
+      this.scene
+    )
+    sphere.position.y = 1
+    const box = BABYLON.MeshBuilder.CreateBox(
+      'box',
+      { height: 2, width: 2, depth: 2 },
+      this.scene
+    )
+    box.position.set(1, 1, 3)
+    const ground = BABYLON.MeshBuilder.CreateGround(
+      'myGround',
+      { width: 30, height: 30, subdivisions: 32 },
+      this.scene
+    )
+    const myMaterial = new BABYLON.StandardMaterial('myMaterial', this.scene)
+    myMaterial.specularColor = new BABYLON.Color3(0, 0, 0)
+    myMaterial.diffuseColor = new BABYLON.Color3(0.42, 0.83, 0.23)
+    ground.material = myMaterial
+    const csg = new BABYLON.CascadedShadowGenerator(1024, sun)
+    csg.usePercentageCloserFiltering = true
+    csg.autoCalcDepthBounds = true
+    csg.autoCalcDepthBoundsRefreshRate = 4
+    csg.addShadowCaster(sphere)
+    csg.addShadowCaster(box)
+    ground.receiveShadows = true
+
+    this.engine.runRenderLoop(() => this.render())
+
+    // events
     window.addEventListener('keydown', (e) => this.keydown(e), true)
     window.addEventListener('keyup', (e) => this.keyup(e), true)
-    this.animate()
   },
   destroyed() {
-    this.renderer.dispose()
+    this.scene.dispose()
+    this.engine.dispose()
   },
   methods: {
-    animate() {
-      requestAnimationFrame(this.animate)
-      const dir = new THREE.Vector3()
-      if (this.up) dir.x++
-      if (this.down) dir.x--
-      if (this.left) dir.z--
-      if (this.right) dir.z++
-      this.cube.position.add(dir.normalize().multiplyScalar(0.08))
-      this.raycaster.set(
-        new THREE.Vector3(this.cube.position.x, 2, this.cube.position.z),
-        new THREE.Vector3(0, -1, 0)
-      )
-      for (const intersect of this.raycaster.intersectObject(this.plane)) {
-        this.cube.position.y = intersect.point.y + 0.5
-      }
-      // this.controls.update()
-      this.renderer.render(this.scene, this.camera)
+    render() {
+      this.scene.render()
     },
     keydown(e) {
       switch (e.keyCode) {
