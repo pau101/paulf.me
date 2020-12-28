e87c72e44808352dffdb8c26a3d867c2a60659a3
Paul Fulham <paul.fulham0@gmail.com>
2020-12-28 10:31:21 -0800

    Add blank mods page

---
+++ pages/games/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main aria-role="main">
+    <div>
+      <h1>Nothing here yet...</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
