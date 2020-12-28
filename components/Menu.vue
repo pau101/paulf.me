90eba641005307e87dfd81243a8002e0b04d8f1e
Paul Fulham <paul.fulham0@gmail.com>
2020-12-28 11:13:29 -0800

    Update menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -2,8 +2,12 @@
   <nav class="menu">
     <ul class="menu__inner">
       <li><a href="/about">About</a></li>
-      <li><a href="/posts">Posts</a></li>
-      <li><a href="/games">Games</a></li>
+      <!--<li><a href="/posts">Posts</a></li>-->
+      <li>
+        <a href="https://www.curseforge.com/members/pau101/projects">
+          Projects
+        </a>
+      </li>
     </ul>
   </nav>
 </template>
