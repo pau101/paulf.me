9ef281aa4eefd3f5c3a151c515021d9f66e18353
Paul Fulham <paul.fulham0@gmail.com>
2020-12-28 11:09:30 -0800

    Update about section

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -13,16 +13,109 @@
           Oregon. I have special interests in software security, machine
           learning, and reverse engineering.
         </p>
-        <h2 id="jobs">Jobs</h2>
+        <h3 id="skills" style="color: #2f7fc9;">SKILLS</h3>
         <p>
-          Since 2013: <strong>Self employed Minecraft mod developer</strong>
+          <strong>Languages:</strong>
+          <br />
+          Java, Javascript, Rust, C#, C++, Python, SQL
         </p>
-        <h2 id="education">Education</h2>
         <p>
-          <strong>Eastern Washington University &mdash; Cheney, WA</strong>
+          <strong>Tools &amp; Technologies:</strong>
+          <br />
+          Git, IntelliJ IDEA, Visual Studio, Node.js, MySQL
+        </p>
+        <h3 id="jobs" style="color: #2f7fc9;">EXPERIENCE</h3>
+        <p>
+          <strong>Self Employed</strong> &dash; <i>Minecraft Mod Developer</i>
+          <br />
+          <span style="opacity: 75%">March 2016 &dash; PRESENT</span>
         </p>
         <ul>
-          <li>Bachelor of Computer Science (2015 - 2019)</li>
+          <li>
+            Develop and maintain custom server game mode for collaborative
+            YouTube video series “X-Life” (averaging 1,000,000+ monthly views)
+          </li>
+          <li>
+            Ported in-game web image viewing mod to latest Minecraft version
+          </li>
+          <li>
+            Developed custom content for video series “FunCraft”
+          </li>
+          <li>
+            Developed new NPC procedural appearance randomization, behavior, and
+            relationship progression system for “Village Box” for video series
+            “100 Baby Challenge”
+          </li>
+          <li>
+            Provided tutoring for mod development
+          </li>
+          <li>
+            Developed player moderation tools using a centralized block edit
+            history database
+          </li>
+        </ul>
+        <p>
+          <strong>CurseForge Rewards Program</strong> &dash;
+          <i>Minecraft Mod Author</i>
+          <br />
+          <span style="opacity: 75%">November 2014 &dash; PRESENT</span>
+        </p>
+        <ul>
+          <li>
+            Research and provide mitigations for network security issues
+          </li>
+          <li>
+            Improved dynamic terrain navigation animations for horse drawn
+            vehicles in “AstikorCarts” as new project maintainer
+          </li>
+          <li>
+            Designed blend system for procedural winged flight animation in
+            “Wings”
+          </li>
+          <li>
+            Developed static code analysis tool for identifying possible null
+            deference issues
+          </li>
+          <li>
+            Developed 3D model conversion tools for working with various formats
+          </li>
+          <li>
+            Develop and maintain a physically modelled decorative hanging lights
+            mod “Fairy Lights” implementing optimized curve collision detection
+          </li>
+          <li>
+            Implemented camera perspective system and designed a procedural
+            IK-driven boat animations for “The Betweenlands”
+          </li>
+          <li>
+            Designed procedural noise-driven cave and tree generation algorithms
+            for “The Betweenlands” dimension
+          </li>
+          <li>
+            Developed improved path navigation and extended the animation system
+            for boss fights in “Mowzie’s Mobs”
+          </li>
+          <li>
+            Earned 12,000,000+ downloads
+          </li>
+        </ul>
+        <h3 id="education" style="color: #2f7fc9;">EDUCATION</h3>
+        <p>
+          <strong>Eastern Washington University &mdash; Cheney, WA</strong>
+          <i>&dash; Bachelor of Computer Science</i>
+          <br />
+          <span style="opacity: 75%">September 2015 &dash; June 2019</span>
+        </p>
+        <ul>
+          <li>Academic Honors: Dean’s List (7 quarters)</li>
+          <li>
+            Built AR mobile racing application as senior capstone project and
+            represented university at Microsoft’s TEALS Computer Science Fair
+          </li>
+          <li>
+            Discovered and presented research on a severe pre-authentication
+            Minecraft network DoS vulnerability
+          </li>
         </ul>
       </div>
     </article>
